bdce940c223c6ade425b3b94f55e481c1cd992fa
Mark <mark.scrudder@gmail.com>
2021-07-08 19:46:04 -0700

    Changed coloration for easier viewing.

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Square = ({ value, onClick, cMenu }) => {
 
-  let className = 'square ' + value.shade;
+  let className = 'square ' + value.shade + ' ' + value.side;
 
   return(
     <div className={className} onClick={onClick} onContextMenu={cMenu} >
